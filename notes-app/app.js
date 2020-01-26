c781885d76e1bd14a3de641c9cef124ac28351f0
Kelly H <kellyaccnts@gmail.com>
2020-01-26 14:17:54 -0700

    added yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,55 @@
 const validator = require("validator");
+const yargs = require("yargs");
 const chalk = require("chalk");
 const add = require("./utils.js");
 
-const sum = add(7, 2);
-console.log(sum);
+// const command = process.argv;
+// console.log(yargs.argv);
 
-//
-console.log(chalk.inverse.green(validator.isEmail("egg@isegg.com")));
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: "Note Title",
+            demandOption: true,
+            type: "string"
+        },
+        body: {
+            describe: "Note Body",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: argv => {
+        console.log("Title: " + argv.title);
+        console.log("Body:" + argv.body);
+    }
+});
+
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: () => {
+        console.log("Called remove");
+    }
+});
+
+yargs.command({
+    command: "list",
+    describe: "List all notes",
+    handler: () => {
+        console.log("Called list");
+    }
+});
+
+yargs.command({
+    command: "read",
+    describe: "Read a note",
+    handler: () => {
+        console.log("Called read");
+    }
+});
+
+// We need to access yargs for it to work correctly
+yargs.parse();
