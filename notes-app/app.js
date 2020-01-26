17ea0665df0e63a1401bfac77bf26a6f7dacb424
Kelly H <kellyaccnts@gmail.com>
2020-01-25 23:51:00 -0700

    complete section 3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,9 @@
+const validator = require("validator");
+const chalk = require("chalk");
 const add = require("./utils.js");
 
 const sum = add(7, 2);
 console.log(sum);
+
+//
+console.log(chalk.inverse.green(validator.isEmail("egg@isegg.com")));
