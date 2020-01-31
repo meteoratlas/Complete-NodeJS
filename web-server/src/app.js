059db3f231574213cc71da8b550209d2cd70156b
Kelly H <kellyaccnts@gmail.com>
2020-01-30 22:04:15 -0700

    basic styling

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -37,6 +37,22 @@ app.get("/weather", (req, res) => {
     res.send({ location: "Your City", temperate: 4.02 });
 });
 
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        errorMessage: "404 - no such help article exists.",
+        title: "404",
+        name: "Kelly"
+    });
+});
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        errorMessage: "404 - requested page could not be found.",
+        title: "404",
+        name: "Kelly"
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
