9f61d77f120de6fad85f2adbc6ac077d914fa6c9
Kelly H <kellyaccnts@gmail.com>
2020-02-10 23:13:12 -0700

    finish weather app ui & custom search

---
+++ web-server/public/js/index.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form");
+const search = document.querySelector("input");
+const msg1 = document.querySelector("#msg1");
+const msg2 = document.querySelector("#msg2");
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    msg1.textContent = "Searching...";
+    msg2.textContent = "";
+
+    fetch(`http://localhost:3000/weather?address=${search.value}`).then(
+        response => {
+            response.json().then(data => {
+                if (data.error) {
+                    msg1.textContent = data.error;
+                } else {
+                    msg1.textContent = data.location;
+                    msg2.textContent = data.forecast;
+                }
+            });
+        }
+    );
+});
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -40,7 +40,7 @@ app.get("/weather", (req, res) => {
     }
     utils.geocode(
         req.query.address,
-        (error, { latitude, longitude, location }) => {
+        (error, { latitude, longitude, location } = {}) => {
             if (error) {
                 return res.send(error);
             }
